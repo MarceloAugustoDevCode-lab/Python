93bec0bc11ed89446ed7a9548474806847af7e2b
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-09-19 12:09:41 -0300

    Initial commit

---
+++ Desafios/01.py
@@ -0,0 +1,6 @@
+# Desafio 01
+
+#Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de Boas-Vindas de acordo com o valor digitado.
+nome = input( 'Qual seu nome? ')
+print('Bem-Vindo', nome)
+
---
+++ Desafios/02.py
@@ -0,0 +1,9 @@
+# Desafio 02
+
+# Crie um script Python que leia o dia, o Mês e o Ano de nacimento de uma pessoa e mostre uma mensagem com a data formatada.
+
+dia = input('qual seu Dia de nascimento? ')
+mes = input('qual seu Mês de nascimento? ')
+ano = input('qual seu Ano de nascimento?')
+
+print('Sua data de Nascimento é {}/{}/{}'.format( dia, mes, ano))
