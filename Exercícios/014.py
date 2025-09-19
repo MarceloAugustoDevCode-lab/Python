dc20634a9d3a4093f44566c4a1ac4394b4c9ab0b
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-09-19 13:35:52 -0300

    Initial commit

---
+++ Exercícios/014.py
@@ -0,0 +1,7 @@
+#Exercício Python 14:
+# Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+
+Celsius = float(input('Digite a temperatura em Celsius: '))
+Fahrenheit = (Celsius * 9/5) + 32
+
+print('O resultado é ',Fahrenheit,' de Fahrenheit.')
