9c108cbe106991647bb8a4f797697140a7f4c90e
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-09-20 20:40:48 -0300

    Initial commit

---
+++ Aula 5 - Utilizando Módulos/Utilizando Módulos.py
@@ -0,0 +1,34 @@
+# Trabalhando com Modulos.
+
+# Bibliotecas podemos fazer importação no Python.
+# Dentro da linguagem python incluir alguma coisa tenho que usar o comando  Import.
+
+# exemplo, math em português e matematica.
+
+import math
+
+# mais se eu quiser so uma funcionalidade de math em vez de usar toda biblioteca. eu uso o from.
+
+from math import pow # e potencia.
+
+# algumas funções do math
+
+# ceil - ele redonda pra cima.
+# floor - ele redonda pra baixo.
+# trunc - truncar o numero ele vai eliminar da , pra frente.
+# pow - e a potencia.
+# sqrt - e para calcular a raiz quadrada.
+# factorial - e usada para calculo fatorial.
+
+# e se eu quiser import duas ficara dessa forma.
+
+from math import sqrt,floor
+
+# pratica
+
+
+
+
+
+
+
