5b06c63a2da13fb7fdf6ba57f915580f636c3a76
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-09-21 15:13:14 -0300

    Initial commit

--- Exercícios/019 - Um Professor quer sortear um dos seus quatro alunos para apagar o quadro. faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido..py
+++ Exercícios/019 - Um Professor quer sortear um dos seus quatro alunos para apagar o quadro. faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido..py
@@ -1,2 +1,13 @@
 #019
 #Um Professor quer sortear um dos seus quatro alunos para apagar o quadro. faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+
+import random
+
+aluno1 = input('digite o nome do primeiro aluno? ')
+aluno2 = input('digite o nome do segundo aluno? ')
+aluno3 = input('digite o nome do terceiro aluno? ')
+aluno4 = input('digite o nome do quarto aluno? ')
+
+lista = [aluno1, aluno2, aluno3, aluno4]
+sorteio = random.choice(lista)
+print('o aluno escolhido foi {}'.format(sorteio))
