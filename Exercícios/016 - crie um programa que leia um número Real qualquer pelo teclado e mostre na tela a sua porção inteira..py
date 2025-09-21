8c08f20e4771ebc698b98180979d390e7e50f3c2
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-09-21 13:23:29 -0300

    Initial commit

--- Exercícios/016 - crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira..py
+++ Exercícios/016 - crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira..py
@@ -1,6 +1,10 @@
 #016 - crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira.
 #exemplo: Digite um número: 6.127 O número 6.127 tem a parte inteira 6.
 
+import math
 
+num = float(input('Digite o numero: '))
+inteiro = math.trunc(num)
+print('O numero de porção intera é {}'.format(inteiro))
 
 
