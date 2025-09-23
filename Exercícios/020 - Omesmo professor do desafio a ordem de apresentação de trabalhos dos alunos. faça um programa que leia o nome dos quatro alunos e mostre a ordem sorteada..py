470cd9e27e4e086f235092403baa1fc7c86c5c36
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-09-23 13:40:39 -0300

    Initial commit

--- Exercícios/020 - Omesmo professor do desafio a ordem de apresentação de trabalhos dos alunos. faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada..py
+++ Exercícios/020 - Omesmo professor do desafio a ordem de apresentação de trabalhos dos alunos. faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada..py
@@ -1,2 +1,20 @@
 #020
 # mesmo professor do desafio a ordem de apresentação de trabalhos dos alunos. faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+
+import random
+
+aluno1 = input('digite o nome do primeiro aluno? ')
+aluno2 = input('digite o nome do segundo aluno? ')
+aluno3 = input('digite o nome do terceiro aluno? ')
+aluno4 = input('digite o nome do quarto aluno? ')
+
+lista = [aluno1, aluno2, aluno3, aluno4]
+sorteio = random.choice(lista)
+listanomes = random.shuffle(lista)
+print('o aluno escolhido foi {}'.format(sorteio))
+print('A lista da ordem {}'.format(lista))
+
+
+
+
+
