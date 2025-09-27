360aaad55d95145f30ed47e5b237bb2d5a949ccd
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-09-27 14:08:02 -0300

    Initial commit

---
+++ Exercícios/027 - Primeiro e ultimo nome de uma pessoa.py
@@ -0,0 +1,12 @@
+#027 - Crie um programa que leia o nome de uma pessoa, mostrando em seguida o primeiro e o ultimo nome separadamente
+#ex ana maria de souza
+#primeiro = ana
+#ultimo = souza
+
+#marcelo augusto de jesus
+
+nome = str(input('Digite seu nome completo: '))
+nome = nome.upper()
+nome = nome.split()
+print('O Primeiro nome é ',nome[0])
+print('O Segundo nome é',nome[1])
