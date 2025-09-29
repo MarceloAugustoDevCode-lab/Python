1ffac0d54f523b2744c48c6ad8beea12afa03610
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-09-29 10:39:33 -0300

    Initial commit

---
+++ Exercícios/029 - Radar eletrônico SIMPLES.py
@@ -0,0 +1,16 @@
+#029 - Escreva um programa que leia a velocidade de um carro.
+# se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado.
+# A multa vai custar R$7,oo reias por cada Km acima do limite.
+
+velocida = float(input('velocidade do Carro ? '))
+limite = 80
+reais = 7
+multa = velocida-limite
+multa = multa * reais
+print(multa) # mostra o calculo da multa
+
+
+if velocida > limite:
+    print('Você foi Multado em R$ {} Reais pois estava acima de {}Km/h.'.format(multa,limite))
+else:
+    print('')
