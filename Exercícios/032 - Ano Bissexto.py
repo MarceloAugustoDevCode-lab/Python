a5278bbbc2e1eef9087a3c538efb6e3413b7879f
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-09-29 18:38:29 -0300

    Initial commit

---
+++ Exercícios/032 - Ano Bissexto.py
@@ -0,0 +1,12 @@
+#032 - Faça um programa que leia um ano qualquer e mostra se ele é BISSEXTO.
+
+from datetime import date
+
+ano = int(input('Quantos anos na mesma? '))
+if ano == 0:
+    ano = date.today().year
+
+if  ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('Portanto o ano {} é bissexto.'.format(ano))
+else:
+    print('Portanto o ano {} não é bissexto'.format(ano))
