00185c9ac95bcd9de52c21ef3bdb1143112b99fc
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-09-29 10:06:52 -0300

    Initial commit

--- Aula 06 - Condições em Python/Aula 06 - Condições em Python.py
+++ Aula 06 - Condições em Python/Aula 06 - Condições em Python.py
@@ -1,5 +1,6 @@
 #estrutura de sequencia: condicionais.
 
+'''
 #vou explicar de modo rapido. primeiro vou criar uma variavel com nome de nome:
 nome = input('Digite seu nome: ')
 # depois criar outra variavel com nome de escolha para dar a função do if(se) e else(senão)
@@ -12,7 +13,7 @@ if escolha == 'maiúsculo':# se escolha for (==) igual string 'maiúsculo' ficar
 #senão eu quero que nome fique Minúsculo o vou escrever qualquer outra se Minúsculo, else no python e (senão).
 else:#senão a escolha sera minúsculo
     print(nome.lower())#função que altera para minúsculo
-
+'''
 '''
 tempo = int(input('Quantos anos tem seu carro ? '))
 
@@ -30,5 +31,61 @@ tempo = int(input('Quantos anos tem seu carro ? '))
 print('carro novo'if tempo <=3 else 'carro velho'))
 print('--FIM--')
 '''
+'''
+# Condicional Simples:-------------------------
+nome = str(input('Digite seu nome: '))
+if nome == 'Marcelo':
+    print('Que nome lindo você tem!')
+print('Bom dia, {}!'.format(nome))
+#----------------------------------------------
+'''
+'''
+# Condicional Composta:-------------------------
+nome = str(input('Digite seu nome: '))
+if nome == 'Marcelo':
+    print('Que nome lindo você tem!')
+else:
+    print('Seu nome é tão normal!')
+print('Bom dia, {}!'.format(nome))
+#-----------------------------------------------
+'''
+'''
+# Media do aluno:
+n1 = float(input('Digite sua primeira nota: ')) 
+n2 = float(input('Digite sua segundo nota: '))
+m = (n1 + n2) / 2
+if m >= 6 :
+    print('Sua media foi boa! Parabens!')
+else:
+    print('Sua media foi ruim! Estude mais!')
+'''
+# ou
+'''
+# Media do aluno:
+n1 = float(input('Digite sua primeira nota: ')) 
+n2 = float(input('Digite sua segundo nota: '))
+m = (n1 + n2) / 2
+print('Sua media foi boa! Parabens'if m >=6 else 'Sua media foi ruim! Estude mais!')
+'''
+#Exercicios.
+#028 - Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador.
+#O programa deverá escrever na tela se o usuário venceu ou perdeu.
 
+#029 - Escreva um programa que leia a velocidade de um carro.
+# se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado.
+# A multa vai custar R$7,oo reias por cada Km acima do limite.
 
+#030 - Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou Impar.
+
+#031 - Desenvolva um programa que pergunte a distancia de uma viagem em Km.
+#Calcule o preço da passagem,cobrado R$0,50 por Km para viagens de até 200Km e R$0,45 para viagens mais longas.
+
+#032 - Faça um programa que leia um ano qualquer e mostra se ele é BISSEXTO.
+
+#033 - Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+
+#034 - Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento.
+#para salários superiores a R$1.250.00, calcule um aumento de 10%
+#para os inferiores ou iguais e aumento e de 15%.
+
+#035 Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
---
+++ Exercícios/028 - Jogo da Adivinhação.py
@@ -0,0 +1,12 @@
+#028 - Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador.
+#O programa deverá escrever na tela se o usuário venceu ou perdeu.
+
+import random
+
+adivinhar = random.randint(1, 5)
+print(adivinhar)# VOCÊ VER SE O NUMERO SORTEADO. para testar o codigo.
+numero = int(input('tente adivinhar o numero ?'))
+if numero == adivinhar:
+    print('Parabens você venceu!')
+else:
+    print('Você perdeu!')
