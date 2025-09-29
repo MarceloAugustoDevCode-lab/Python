d73e81061c031099eed4c229c9da1f3c3247b436
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-09-29 12:43:02 -0300

    Initial commit

---
+++ Exercícios/030 - Par ou ímpar.py
@@ -0,0 +1,6 @@
+#030 - Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou Impar.
+numero = int(input('digite um numero: '))
+if numero % 2 == 0:
+    print('par')
+else:
+    print('impar')
