1053a155cf76d20997c86985fa54ad93324df8f0
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-09-29 19:51:30 -0300

    Initial commit

--- Exercícios/033 - Maior e menor valores.py
+++ Exercícios/033 - Maior e menor valores.py
@@ -1,52 +1,41 @@
 #033 - Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
 
-numero = int(input('Digite o primeiro numero: '))
-numero2 = int(input('Digite o segundo numero: '))
-numero3 = int(input('Digite o terceiro numero: '))
-# primeiro numero
+numero = int(input('Digite o primeiro valor: '))
+numero2 = int(input('Digite o segundo valor: '))
+numero3 = int(input('Digite o terceiro valor: '))
+
+
+#verifica menor
 if numero < numero2:
-    print('O primeiro numero e menor que o segundo numero!')
-elif numero > numero2:
-    print('O primeiro numero e maior que o segundo numero!')
-elif numero == numero2:
-    print('O primeiro numero e igual que o segundo numero!')
-if numero < numero3:
-    print('O primeiro numero e menor que o terceiro numero!')
-elif numero > numero3:
-    print('O primeiro numero e maior que o terceiro numero!')
-elif numero == numero3:
-    print('O primeiro numero e igual que o terceiro numero!')
-
-
-
-# segundo numero
-if numero2 < numero3:
-    print('O segundo numero e menor que o terceiro numero!')
+    print('O menor valor digitado! {}'.format(numero))
+elif numero2 < numero3:
+    print('O menor valor digitado! {}'.format(numero2))
+else:
+    print('O menor valor digitado! {}'.format(numero3))
+#verifica maior
+if numero > numero2:
+    print('O maior valor digitado! {}'.format(numero))
 elif numero2 > numero3:
-    print('O segundo numero e maior que o terceiro numero!')
-elif numero2 == numero3:
-    print('O segundo numero e igual que o terceiro numero!')
-if numero2 < numero:
-    print('O segundo numero e menor que o primeiro numero!')
-elif numero2 > numero:
-    print('O segundo numero e maior que o primeiro numero!')
-elif numero2 == numero:
-    print('O segundo numero e igual que o primeiro numero!')
+    print('O maior valor digitado! {}'.format(numero2))
+else:
+    print('O maior valor digitado! {}'.format(numero3))
 
+#ou
+'''
+#verifica menor
+menor = numero
+if numero2 < numero and numero2 < numero3:
+    menor = numero2
+if numero3 < numero and numero3 < numero2:
+    menor = numero3
+    
+#verifica maior
+maior = numero
+if numero2 > numero and numero2 > numero3:
+    maior = numero2
+if numero3 > numero and numero3 > numero2:
+    maior = numero3
 
-
-
-
-# terceiro numero
-if numero3 < numero2:
-    print('O terceiro numero e menor que o segundo numero!')
-elif numero3 > numero2:
-    print('O terceiro numero e maior que o segundo numero!')
-elif numero3 == numero2:
-    print('O terceiro numero e igual que o segundo numero!')
-if numero3 < numero:
-    print('O terceiro numero e menor que o primeiro numero!')
-elif numero3 > numero:
-    print('O terceiro numero e maior que o primeiro numero!')
-elif numero3 == numero:
-    print('O terceiro numero e igual que o primeiro numero!')
+print('O menor valor digitado foi {}'.format(menor))
+print('O maior valor digitado foi {}'.format(maior))
+'''
