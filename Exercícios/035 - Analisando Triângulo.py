1382791d5eb48819fcd8c0f45c2bfdedd53600be
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-09-29 21:30:26 -0300

    Initial commit

--- Exercícios/035 - Analisando Triângulo.py
+++ Exercícios/035 - Analisando Triângulo.py
@@ -1,10 +1,10 @@
 #035 Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
 
-r1 = float(input('Digite o comprimento do primeira reta: '))
-r2 = float(input('Digite o comprimento do segunda reta: '))
-r3 = float(input('Digite o comprimento do terceira reta: '))
+reta1 = float(input('Digite o comprimento do primeira reta: '))
+reta2 = float(input('Digite o comprimento do segunda reta: '))
+reta3 = float(input('Digite o comprimento do terceira reta: '))
 
-if r1 + r2 > r3 and r1 + r3 > r2 and r2 + r3 > r1:
+if reta1 + reta2 > reta3 and reta1 + reta3 > reta2 and reta2 + reta3 > reta1:
     print('Os segmentos acima podem formar um triângulo.. sim')
 else:
     print('Os segmentos acima não podem formar um triângulo.')
