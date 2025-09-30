e494ac6d5e6afc1bf215ceae12df88cd92ac1c54
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-09-30 17:52:23 -0300

    Initial commit

--- Exercícios/040 – Aquele clássico da Média.py
+++ Exercícios/040 – Aquele clássico da Média.py
@@ -2,3 +2,15 @@
 #- Média abaixo da 5.0:REPROVADO
 #- Média entra 5.0 a 6.9: RECUPERAÇÃO
 #- Média 7.0 ou superior:APROVADO
+
+nota = int(input('Digite sua Primeira Nota: '))
+nota2 = int(input('Digite sua Segunda Nota: '))
+
+media = (nota + nota2) / 2
+
+if media >= 7:
+    print('\033[1;31;40mAPROVADO {}\033[m'.format(media))
+elif media == 5 and media < 6.9:
+    print('\033[1;31;40mRECUPERAÇÃO {}\033[m'.format(media))
+elif media < 5:
+    print('\033[1;31;40mREPROVADO {}\033[m'.format(media))
