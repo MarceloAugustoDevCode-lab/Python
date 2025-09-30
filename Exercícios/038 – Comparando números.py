ef960045a748f860acc33fc24a26478c6666e56b
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-09-30 15:01:29 -0300

    Initial commit

--- Exercícios/038 – Comparando números.py
+++ Exercícios/038 – Comparando números.py
@@ -1,2 +1,12 @@
 #038 - Escreva um programa que leia dois números inteiros a compare-os. mostrando na tala uma mensagem:
 # O primeiro valor é maior O segundo valor é maior Não existe valor maior, os dois são iguais
+
+numero = int(input('Digite o primeiro numero: '))
+numero2 = int(input('Digite o segundo numero: '))
+
+if numero > numero2:
+    print('O primeiro e maior \033[1;31;40m{}\033[m'.format(numero))
+elif numero2 < numero:
+    print('O segundo e maior \033[1;31;40m{}\033[m'.format(numero))
+else:
+    print('O primeiro igual ao segundo \033[1;31;40m{}\033[m'.format(numero))
