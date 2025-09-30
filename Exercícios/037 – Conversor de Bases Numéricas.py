e12a6d0c32dc221770c719b268d008b3c3adfbf4
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-09-30 12:29:31 -0300

    Initial commit

--- Exercícios/037 – Conversor de Bases Numéricas.py
+++ Exercícios/037 – Conversor de Bases Numéricas.py
@@ -2,3 +2,19 @@
 #1 para binario
 #2 para octal
 #3 para hexadecimal
+
+numero = int(input('Digite o numero: '))
+escolha = input('Digite o escolha ').split()
+
+if escolha == '1':
+    binario = bin(numero)
+    print(binario)
+    print('\033[1;32;40m{}\033[m'.format(binario))
+elif escolha == '2':
+    octal = oct(numero)
+    print('\033[1;32;40m{}\033[m'.format(octal))
+elif escolha == '3':
+    hexadecimal = hex(numero)
+    print('\033[1;32;40m{}\033[m'.format(hexadecimal))
+else:
+    pass
