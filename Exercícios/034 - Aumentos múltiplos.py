9cbf02e9a0510cc8d382ea1723905ecffbdc6246
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-09-29 21:07:46 -0300

    Initial commit

---
+++ Exercícios/034 - Aumentos múltiplos.py
@@ -0,0 +1,24 @@
+#034 - Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento.
+#para salários superiores a R$1.250.00, calcule um aumento de 10%
+#para os inferiores ou iguais e aumento e de 15%.
+
+
+
+valor = float(input('Digite os Valor do Salario ? '))
+
+
+if valor < 1250:
+    aumento = 15
+    covertdecimal = aumento / 100
+    calculoaumento = valor * covertdecimal
+    novosalario = calculoaumento + valor
+
+    print('O seu antigo salario R${} reais \n O novo salario com {}% de aumento será R${} reais \n Salario Total R${} reais'.format(valor,aumento,calculoaumento,novosalario))
+else:
+    aumento = 10
+    covertdecimal = aumento / 100
+    calculoaumento = valor * covertdecimal
+    novosalario = calculoaumento + valor
+    print('O seu antigo salario R${} reais \n O novo salario com {}% de aumento será R${} reais \n Salario Total R${} reais'.format(valor,aumento,calculoaumento,novosalario))
+
+
