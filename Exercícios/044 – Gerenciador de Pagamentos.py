ccfefcea94dcb02b3798e0185ce593dea20dec90
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-10-01 10:32:26 -0300

    Initial commit

--- Exercícios/044 – Gerenciador de Pagamentos.py
+++ Exercícios/044 – Gerenciador de Pagamentos.py
@@ -3,13 +3,14 @@
 #– à vista no cartão: 5% de desconto
 #– em até 2x no cartão: preço formal
 #– 3x ou mais no cartão: 20% de juros
-
+print('{:=^40}'.format(' \033[1;31;40mLOJAS VAREJO\033[m '))
 valor = int(input('Digite sua valor do Produto: '))
 escolha = str(input('Digite sua escolha: \033[1;31;40mA\033[m a vista ou \033[1;32;40mB\033[m cartão a vista ou \033[1;35;40mC\033[m cartão ate 2x vezes sem juros ou \033[1;34;40mD\033[m em 3x + 20% de juros ?')).upper()
 vista ='A'
 cartao ='B'
 parcela2x='C'
 parcela3x='D'
+
 if escolha == 'A' or escolha == 'B' or escolha == 'C' or escolha == 'D':
     if escolha == 'A':
         avista = valor  * 10 / 100 # porcentagem 30% / 100%
