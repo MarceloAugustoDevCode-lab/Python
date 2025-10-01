151c76709a4d81c8d5b822f319449e256c63a2fb
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-10-01 11:32:25 -0300

    Initial commit

--- Exercícios/045 – GAME Pedra Papel e Tesoura.py
+++ Exercícios/045 – GAME Pedra Papel e Tesoura.py
@@ -1,1 +1,10 @@
 #045 - Cria um programa que faça o computador jogar Jokanpô com você
+
+jogador1 = str(input('Digite o nome do jogador 1: '))
+jogador2 = str(input('Digite o nome do jogador 2: '))
+jogador3 = str(input('Digite o nome do jogador 3: '))
+
+escolhajogador1 = str(input('Digite a sua escolha: '))
+if escolhajogador1 == 'pedra':
+escolhajogador2 = str(input('Digite a sua escolha: '))
+escolhajogador3 = str(input('Digite a sua escolha: '))
