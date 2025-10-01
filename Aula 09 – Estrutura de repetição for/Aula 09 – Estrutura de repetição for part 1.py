51e266cb9a14f0094e14367aee52d4916eaa90ae
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-10-01 14:18:31 -0300

    Initial commit

--- Aula 09 – Estrutura de repetição for/Aula 09 – Estrutura de repetição for part 1.py
+++ Aula 09 – Estrutura de repetição for/Aula 09 – Estrutura de repetição for part 1.py
@@ -23,5 +23,19 @@
 #passo
 #pega
 
+#codigo pega moeda
+# laço c no intervalo(0,3)
+    #se moeda
+        #pega
+    #passo
+    #pula
+#passo
+#pega
 
-
+#for c in range(0,3)
+    #if moeda:
+        #pega
+    #passo
+    #pula
+#passo
+#pega
