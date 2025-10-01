9b0f3d95a1f03e5ddc5bdda69914f8561a231c8d
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-10-01 14:23:16 -0300

    Initial commit

--- Aula 09 – Estrutura de repetição for/Aula 09 – Estrutura de repetição for part 1.py
+++ Aula 09 – Estrutura de repetição for/Aula 09 – Estrutura de repetição for part 1.py
@@ -1,30 +1,31 @@
 #Aula 09 – Estrutura de repetição for parte 1
+
 #em português:
-# laço c no intervalo(1,10)
+# laço c no intervalo(1,10):
     #passo
 #pega
 
 #em inglês:
-#for c in range(1,10)
+#for c in range(1,10):
     #passo
 #pega
 
 #codigo de pula laço
 
-# laço c no intervalo(0,3)
+# laço c no intervalo(0,3):
     #passo
     #pula
 #passo
 #pega
 
-#for c in range(0,3)
+#for c in range(0,3):
     #passo
     #pula
 #passo
 #pega
 
 #codigo pega moeda
-# laço c no intervalo(0,3)
+# laço c no intervalo(0,3):
     #se moeda
         #pega
     #passo
@@ -32,11 +33,17 @@
 #passo
 #pega
 
-#for c in range(0,3)
+#for c in range(0,3):
     #if moeda:
         #pega
     #passo
     #pula
 #passo
 #pega
+
+#Digite print('oi') 8 vezes.
+oi= ''
+for oi in range(0,9):
+    print('\033[1;31;40m{}\033[m'.format(oi), end=' ')
+    pass
 
