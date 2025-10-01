4a8ef19cb795fc5ae0cb122556aa572a76056375
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-10-01 10:12:38 -0300

    Initial commit

--- Exercícios/044 – Gerenciador de Pagamentos.py
+++ Exercícios/044 – Gerenciador de Pagamentos.py
@@ -5,20 +5,27 @@
 #– 3x ou mais no cartão: 20% de juros
 
 valor = int(input('Digite sua valor: '))
-escolha = int(input('Digite sua escolha: '1' a vista ou '2' cartão a vista ou '3' cartão ate 2x vezes sem juros ou em 3x + 20% de juros '))
-if escolha == 1:
-    avista = valor  * 10 / 100 # porcentagem 30% / 100%
-    avista = valor - avista
-    print('à vista dinheiro/cheque: 10% de desconto,TOTAL R${} reais'.format(avista))
-if escolha == 2:
-    cartao = valor * 5 / 100
-    cartao = valor - cartao
-    print('à vista no cartão: 5% de desconto,TOTAL R${} reais'.format(cartao))
-if escolha == 3:
-    parcela2x = valor / 2
-    print('Parcela em até 2x no cartão: preço formal,TOTAL R${} reais'.format(parcela2x))
-if escolha == 4:
-    parcela3x = valor * 20 / 100 # porcentagem 30% / 100%
-    parcela3x = valor + parcela3x
-    parcela3x = (parcela3x /3)
-    print('Parcela em até 3x ou mais no cartão: 20% de juros,TOTAL R${} reais'.format(parcela3x))
+escolha = str(input('Digite sua escolha: A a vista ou B cartão a vista ou C cartão ate 2x vezes sem juros ou D em 3x + 20% de juros ?'))
+vista ='A'
+cartaob ='B'
+parcela2x='C'
+parcela3x='D'
+if escolha == 'A' or escolha == 'B' or escolha == 'C' or escolha == 'D':
+    if escolha == 'A':
+        avista = valor  * 10 / 100 # porcentagem 30% / 100%
+        avista = valor - avista
+        print('à vista dinheiro/cheque: 10% de desconto,TOTAL R${} reais'.format(avista))
+    elif escolha == 'B':
+        cartao = valor * 5 / 100
+        cartao = valor - cartao
+        print('à vista no cartão: 5% de desconto,TOTAL R${} reais'.format(cartao))
+    elif escolha == 'C':
+        parcela2x = valor / 2
+        print('Parcela em até 2x no cartão: preço formal,TOTAL R${} reais'.format(parcela2x))
+    elif escolha == 'D':
+        parcela3x = valor * 20 / 100 # porcentagem 30% / 100%
+        parcela3x = valor + parcela3x
+        parcela3x = (parcela3x /3)
+        print('Parcela em até 3x ou mais no cartão: 20% de juros,TOTAL R${} reais'.format(parcela3x))
+else:
+    print('tente novamente')
