9dacbad36ed41911109882f1719a495dfd353df7
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-10-02 12:15:21 -0300

    Initial commit

--- Exercícios/050 - Soma dos pares.py
+++ Exercícios/050 - Soma dos pares.py
@@ -1,1 +1,9 @@
 #050 - Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
+soma = 0
+for c in range(0,6):
+    numero = int(input('Digite um numero: '))
+    if numero % 2 == 0:
+        soma += numero
+    else:
+        print('Coloque Numero Par')
+print(soma)
