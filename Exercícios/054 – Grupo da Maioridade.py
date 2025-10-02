e35cdcd9a03c1f36f7ae44ecb675a93bdcfdd6b5
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-10-02 20:03:41 -0300

    Initial commit

--- Exercícios/054 – Grupo da Maioridade.py
+++ Exercícios/054 – Grupo da Maioridade.py
@@ -1,1 +1,12 @@
 #054 - Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
+#incompleto
+for c in range(0,2):
+    nome = input('Digite o seu nome: ')
+    ano_nascimento = int(input('Digite ano de nascimento: '))
+    print(nome, ano_nascimento)
+
+if ano_nascimento >= 18:
+
+
+#print(nome, ano_nascimento,end=' ')
+
