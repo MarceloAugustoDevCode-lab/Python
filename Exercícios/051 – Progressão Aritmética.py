787ac647d71d7a80e5f2933f5fafa2fe906cb963
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-10-02 17:22:35 -0300

    Initial commit

--- Exercícios/051 – Progressão Aritmética.py
+++ Exercícios/051 – Progressão Aritmética.py
@@ -1,1 +1,12 @@
 #051 - Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
+print(30*'=')
+print(' 10 Termos de PA ')
+print(30*'=')
+
+primeirotermo = int(input('Primeiro Termo: '))
+razao =int(input('Razao: '))
+decimo = primeirotermo + (10 - 1)* razao
+for c in range(primeirotermo,decimo + razao,razao):
+    print(c,'>',end=' ')
+print('ACABOU!!!')
+
