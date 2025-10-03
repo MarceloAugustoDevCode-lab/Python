68d39363738816a3ba9b985058f51014d8db48a2
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-10-02 22:08:28 -0300

    Initial commit

--- Projetos/mp3 player.py
+++ Projetos/mp3 player.py
@@ -11,18 +11,30 @@ pygame.init()
 
 # Caminho da pasta onde estão as músicas
 arquivos = os.path.join(r"F:\01 Programas Dev\Estudos\Python\som")
+
+
 # lista de nomes musicas mp3.
 arqmp3 = os.listdir(arquivos)
 
+
 # Lista de arquivos dentro da pasta
 lista = list(arqmp3)
-'''sim = 0
+
+sim = 0
 for c in range(0, len(lista)):
     totalcaminho = arquivos + "\\" + lista[c]
     print(totalcaminho)
 totalcaminho = arquivos + "\\" + lista[c == sim]
-'''
-pygame.mixer_music.load(totalcaminho)
+print(totalcaminho)
+
+
+musica = arquivos + "\\" + lista[0]
+musica1 = arquivos + "\\" + lista[1]
+
+pygame.mixer_music.load(musica,musica1)
+print(musica)
+print(musica1)
+
 
 
 play = ''
@@ -33,7 +45,7 @@ proxima = ''
 
 while play != 'stop':
     play = input("Digite o \033[1;31;40m'<'\033[m para voltar \033[1;31;40m'play'\033[m para tocar \033[1;31;40m'>'\033[m para avançar \033[1;31;40m'pause'\033[m para parar e \033[1;31;40m'stop'\033[m para sair: ")
-    musica = c
+    musica = 0
     if play == 'play':
         pygame.mixer.music.play()
     elif play == 'pause':
@@ -41,10 +53,14 @@ while play != 'stop':
     elif play == 'stop':
         pygame.mixer.music.stop()
     elif play == '<':
-
+        pygame.mixer_music.load(musica0)
+        pygame.mixer.music.stop()
         pygame.mixer.music.play()
+        print(musica1)
     elif play == '>':
-
+        pygame.mixer_music.load(musica1)
+        pygame.mixer.music.stop()
         pygame.mixer.music.play()
+        print(musica1)
     else:
         print("Opção inválida. Tente novamente.")
