cb237c3f2763ba7633b6a9c0d5e8b0d10951f72a
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-10-02 21:16:56 -0300

    Initial commit

--- Projetos/mp3 player.py
+++ Projetos/mp3 player.py
@@ -6,20 +6,36 @@
 
 import pygame
 import flet as ft
-
+import os
 
 
 pygame.init()
 # lembrando que as barras tem que ser invertidas "F:\01 Programas Dev\Estudos\Python\som\01.Power.Rangers.mp3".
 # crie uma pasta na pasta principal ou em qualquer pasta do pc e coloque o caminho.
-pygame.mixer.music.load(r"F:\01 Programas Dev\Estudos\Python\som\01.Power.Rangers.mp3")
+
 # "a musica e essa foi que eu comecei a gosta de Informatica."
 
-play = ""
-pause = ""
-stop = ""
+# pygame.mixer.music.load(r"F:\01 Programas Dev\Estudos\Python\som\01.Power.Rangers.mp3")
+arquivos = os.path.join(r"F:\01 Programas Dev\Estudos\Python\som")
+# lista de nomes musicas mp3.
+arqmp3 = os.listdir(arquivos)
+lista = list(arqmp3)
+cont = 0
+for c in range(0, len(lista)):
+    totalcaminho = arquivos + "\\" + lista[c]
+    print(totalcaminho)
+totalcaminho =arquivos + "\\" + lista[c]
+pygame.mixer_music.load(totalcaminho)
+
+
+play = ''
+pause = ''
+stop = ''
+retroceder = ''
+proxima = ''
+
 while play != 'stop':
-    play = input("Digite o \033[1;31;40m'play'\033[m para tocar \033[1;31;40m'pause'\033[m para parar e \033[1;31;40m'stop'\033[m para sair: ")
+    play = input("Digite o \033[1;31;40m'retroceder'\033[m para voltar \033[1;31;40m'play'\033[m para tocar \033[1;31;40m'proxima'\033[m para avançar \033[1;31;40m'pause'\033[m para parar e \033[1;31;40m'stop'\033[m para sair: ")
 
     if play == 'play':
         pygame.mixer.music.play()
@@ -27,5 +43,11 @@ while play != 'stop':
         pygame.mixer.music.pause()
     elif play == 'stop':
         pygame.mixer.music.stop()
+    elif play == 'retroceder':
+            totalcaminho = lista[0]
+            pygame.mixer.music.play()
+    elif play == 'proxima':
+            totalcaminho = lista[+1]
+            pygame.mixer.music.play()
     else:
         print("Opção inválida. Tente novamente.")
