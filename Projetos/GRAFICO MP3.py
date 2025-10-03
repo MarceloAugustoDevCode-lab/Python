22051370624cecb722a92cc6c04dddc14957440a
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-10-02 23:25:46 -0300

    Initial commit

--- Projetos/GRAFICO MP3.py
+++ Projetos/GRAFICO MP3.py
@@ -1,4 +1,4 @@
-
+# interface Flet
 import flet as ft
 
 
@@ -9,11 +9,18 @@ def main(page: ft.Page):
     page.padding = 0
     page.window.width = 450
     page.window.height = 550
-
-
-
-
     page.update()
 
+    #texto
+    page.add(ft.Text('Ola'))
+
+    # container botão ----------------------------------
+    c1 = ft.Container(
+        content = ft.ElevatedButton('Botão'),
+    )
+
+    # adicionando o container
+    page.add(c1)
+    #---------------------------------------------------
 
 ft.app(port=8550,target=main)
