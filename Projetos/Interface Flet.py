266944de484d6d683c754eb93646b50bd2c35f88
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-10-02 23:35:40 -0300

    Initial commit

--- Projetos/Interface Flet.py
+++ Projetos/Interface Flet.py
@@ -17,10 +17,34 @@ def main(page: ft.Page):
     # container botão ----------------------------------
     c1 = ft.Container(
         content = ft.ElevatedButton('Botão'),
+        #bgcolor= "#55555", COR
+        padding= 10,
     )
 
     # adicionando o container
     page.add(c1)
     #---------------------------------------------------
+
+    # container botão ----------------------------------
+    c2 = ft.Container(
+        content = ft.ElevatedButton('>'),
+        #bgcolor= "#55555", COR
+        padding= 10,
+    )
+
+    # adicionando o container
+    page.add(c2)
+    #---------------------------------------------------
+
+    # container botão ----------------------------------
+    c3 = ft.Container(
+        content=ft.ElevatedButton('<'),
+        # bgcolor= "#55555", COR
+        padding=10,
+    )
+
+    # adicionando o container
+    page.add(c3)
+    # ---------------------------------------------------
 
 ft.app(port=8550,target=main)
