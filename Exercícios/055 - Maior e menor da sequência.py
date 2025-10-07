e3e6ee9407065895cf1af2c17be18789c9d7203f
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-10-07 11:44:51 -0300

    Initial commit

--- Exercícios/055 - Maior e menor da sequência.py
+++ Exercícios/055 - Maior e menor da sequência.py
@@ -1,14 +1,16 @@
 #055 - Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
-
-peso = 0
+c = 0
+maior = 0
+menor = 0
 for c in range(1,3+1):
-    pessoas = int(input(f'Digite o maior peso {c}° pessoa: '))
-    if pessoas > peso:
+    peso = int(input(f'Digite o maior peso {c}° pessoa: '))
+    if c == 1:
         maior = peso
-    else:
         menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
 
-
-
-print(f'Total de pessoas com mais de 18 anos são {maior}')
-print(f'Total de pessoas com menos de 18 anos são {menor}')
+print(f'Total de pessoas com mais de 18 anos são {maior}\nTotal de pessoas com menos de 18 anos são {menor}')
