e25f9e9fb6c5398135c379dfc14ca4de06678eb0
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-10-07 13:56:33 -0300

    Initial commit

--- Exercícios/056 – Analisador completo.py
+++ Exercícios/056 – Analisador completo.py
@@ -1,20 +1,28 @@
 #56 - Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do programa, mostre: a média de idade do grupo, qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
 c =0
+soma = 0
 media = 0
-for c in range(1,2+1): # inicio, fim, passo:
+for c in range(1,4+1): # inicio, fim, passo:
     nome = str(input(f'Digite o nome {c}° Pessoa: ')).strip().upper()
     idade = int(input(f'Digite o a Idade da {c}° pessoa: '))
     sexo = str(input(f'Digite o sexo (M/F): ')).strip().upper()
     # media do grupo
     media += idade# soma os numeros idade
     # homem mais velho
-    if c ==1 or idade > idade:
+    if c ==1 or idade > idade or sexo == 'Mm':
         nomev = nome
         idadev = idade
         sexov = sexo
     vnome = nomev
     vidade = idadev
     vsexo = sexov
+    # total de mulheres abaixo dos 20 anos.
+
+    if sexo in 'Ff' or idade < 20:
+        soma += 1
+    somaf = soma
+soma1 = somaf
 media1 = media / 4 #divide os numeros da media.
 print(f'O homem mais velho e {nomev} tem {idadev} anos e do sexo {vsexo}. ')
 print(f'Qual e a Media do grupo: {media1}')
+print(f'Ao todo são {soma1} com menos de 20 anos.')
