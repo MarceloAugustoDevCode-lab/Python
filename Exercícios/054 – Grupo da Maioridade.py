98915adabdc291b38f01c0be598e5223023f5f3b
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-10-07 11:28:30 -0300

    Initial commit

--- Exercícios/054 – Grupo da Maioridade.py
+++ Exercícios/054 – Grupo da Maioridade.py
@@ -2,21 +2,21 @@
 import datetime
 
 ano_atual = datetime.date.today().year
-
+c =0
 maior = 0
 menor = 0
-for c in range(1,7+1):
+for c in range(1,2+1):
     ano_nascimento = int(input(f'Digite o ano de {c}° nascimento: '))
     idade = ano_atual - ano_nascimento
     if idade > 18:
         maior+=1
-    else:
+
+    if idade < 18:
         menor+=1
+maiorv = maior
+menorv = menor
+print(f'Total de pessoas com mais de 18 anos são {maiorv}\nTotal de pessoas com menos de 18 anos são {menorv}')
 
-
-
-print(f'Total de pessoas com mais de 18 anos são {maior}')
-print(f'Total de pessoas com menos de 18 anos são {menor}')
 
 
 
--- Exercícios/055 - Maior e menor da sequência.py
+++ Exercícios/055 - Maior e menor da sequência.py
@@ -1,1 +1,14 @@
 #055 - Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
+
+peso = 0
+for c in range(1,3+1):
+    pessoas = int(input(f'Digite o maior peso {c}° pessoa: '))
+    if pessoas > peso:
+        maior = peso
+    else:
+        menor = peso
+
+
+
+print(f'Total de pessoas com mais de 18 anos são {maior}')
+print(f'Total de pessoas com menos de 18 anos são {menor}')
