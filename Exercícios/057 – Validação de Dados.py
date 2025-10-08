589e548b065d3115e9f63d356536f678ce8b064d
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-10-08 19:31:15 -0300

    Initial commit

--- Exercícios/057 – Validação de Dados.py
+++ Exercícios/057 – Validação de Dados.py
@@ -1,6 +1,6 @@
 #057 - Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores ‘M’ ou ‘F’. Caso esteja errado, peça a digitação novamente até ter um valor correto.
 #a minha solução:
-
+'''
 para = 'S'
 contador = 0
 while para == 'S':
@@ -16,3 +16,11 @@ while para == 'S':
         para == 'S'
     else:
         print('Continue!')
+'''
+#ou
+
+
+sexo = str(input('Digite seu sexo: [M/F]: ')).strip().upper()[0]
+while sexo not in 'MmFf':
+    sexo = str(input('Digite os dados corretos, Por favor informe seu sexo:[M/F]: ')).strip().upper()[0]
+print(f'sexo: {sexo} Registrado com sucesso!')
