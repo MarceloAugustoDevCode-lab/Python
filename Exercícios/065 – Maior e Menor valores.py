6820191ced44f433273089326b8544cf056e0d30
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-10-09 13:27:34 -0300

    Initial commit

--- Exercícios/065 – Maior e Menor valores.py
+++ Exercícios/065 – Maior e Menor valores.py
@@ -1,17 +1,18 @@
 #065 - Crie um programa que leia vários números inteiros pelo teclado.No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos.O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
+from dearpygui.dearpygui import reset_pos
 from django.template.context_processors import media
 
-n = 1
+resposta = 'Ss'
+n = 0
 c = 0
 total = 0
 maior = 0
 menor = 0
 media = 0
-while n != 999:
+while resposta in 'Ss':
     c +=1
-    n = int(input(f'Digite {c}° numero [999 para parar]: '))
-    if n == 999:
-        continue
+    n = int(input(f'Digite {c}° numero: '))
+
     total += n
     media = total / 2
     if c == 1:
@@ -21,6 +22,7 @@ while n != 999:
             maior = n
         if n < menor:
             menor = n
+    resposta = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
 print(f'Você digitou {c-1} números e a soma dos numeros: {total}\nA media do total dos numeros {media}')
 print(f'O maior número foi {maior} e o menor foi {menor}')
 
