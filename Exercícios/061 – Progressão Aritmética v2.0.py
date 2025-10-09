4af564a2867219808e09392071bc85d5d5198ae9
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-10-09 08:22:27 -0300

    Initial commit

--- Exercícios/061 – Progressão Aritmética v2.0.py
+++ Exercícios/061 – Progressão Aritmética v2.0.py
@@ -6,7 +6,14 @@ print(30*'=')
 primeirotermo = int(input('Primeiro Termo: '))
 razao =int(input('Razao: '))
 decimo = primeirotermo + (10 - 1)* razao
-for c in range(primeirotermo,decimo + razao,razao):
-    print(c,'>',end=' ')
-print('ACABOU!!!')
-#imcompleto
+
+
+# Progressão aritmética com while (exemplo: 2, 4, 6, ..., 10)
+n = 10   # Número de termos
+contador = 1
+termo = primeirotermo
+while contador <= n: #contador é menor e igual n.
+    print(termo,end=' ')
+    termo += razao
+    contador += 1
+print('FIM!!!')
