b3fb3f48186c63364cfb4a79aac83f0cc00d0f65
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-10-10 19:19:26 -0300

    Initial commit

--- Exercícios/067 – Tabuada v3.0.py
+++ Exercícios/067 – Tabuada v3.0.py
@@ -2,16 +2,30 @@
 
 c = 0
 valor = 1
+print('-'*30)
 while True:
     if c >= 10:
         c =0
-        valor = int(input('Digite um numero: [Digite numero negativo para sair] '))
+        valor = int(input('Digite a tabuada de qual valor: [Digite numero negativo para sair] '))
     elif c == 0:
-        valor = int(input('Digite um numero: [Digite numero negativo para sair] '))
+        valor = int(input('Digite a tabuada de qual valor: [Digite numero negativo para sair] '))
     if valor < 0:
         break
     c +=1
     total = c * valor
     print(f'{c} X {valor} = {total}')
-
+print('-'*30)
+print('PROGRAMA ENCERRADO')
+'''
+#ou
+while True:
+    n = int(input('Digite a tabuada de qual valor: '))
+    print('-'*30)
+    if n < 0:
+        break
+    for c in range(1, 11):
+        print(f'{n} X {c} = {n * c}')
+    print('-'*30)
+print('PROGRAMA ENCERRADO')
+'''
 
