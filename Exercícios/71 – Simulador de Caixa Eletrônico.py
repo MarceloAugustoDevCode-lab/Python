124b9cb5157873e5bc045fd30a8abe525c343ffe
MarceloAugustoDevCode-lab <marcelo.augusto.devcode@gmail.com>
2025-10-11 13:48:30 -0300

    Initial commit

--- Exercícios/71 – Simulador de Caixa Eletrônico.py
+++ Exercícios/71 – Simulador de Caixa Eletrônico.py
@@ -5,19 +5,37 @@
 contagem = 0
 cedulas1 = 1
 cedulas10 = 10
+cedulas20 = 20
 cedulas50 = 50
 valor = 0
+
 print(30*"=")
 print("CAIXA ELETRONICO")
 print(30*"=")
 
-
 contagem += 1
-total = contagem + valor
 valor = int(input("Digite o valor do saque? R$ "))
-if total == valor :
-    contagem += 1
-    cedulas50 += 50
-    print(f'Total de {contagem} cedulas de R$ {cedulas50}')
+resto1 = valor // cedulas50
+print(resto1)
+
+resto2 = resto1
+resto2 = resto1 // cedulas20
+print(resto2)
+
+resto3 = resto2
+resto3 = resto2 // cedulas10
+print(resto3)
+
+resto4 = resto3
+resto4 = resto3 // cedulas1
+print(resto4)
+
+
+
+
+print(f'Total de {resto1} cedulas de R$ 50')
+print(f'Total de {resto2} cedulas de R$ 20')
+print(f'Total de {resto3} cedulas de R$ 10')
+print(f'Total de {resto4} cedulas de R$ 1')
 
 
