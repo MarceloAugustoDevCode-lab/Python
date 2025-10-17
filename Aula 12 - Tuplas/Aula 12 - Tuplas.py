b0b1abfbc6e5ecb91a6c857dfc9950bd58f4cd43
Marcelo Augusto <marcelo.augusto.devcode@gmail.com>
2025-10-17 11:59:51 -0300

    initial commit

---
+++ Aula 12 - Tuplas/Aula 12 - Tuplas.py
@@ -0,0 +1,8 @@
+# Variavés composta: TUPLAS
+#variavel simples:
+lanche = Hamburgue
+
+#variavel composta a tuplas, lista e dicionario : vamos usar tupla
+
+lanche1 = 'hamburgue', 'suco','pizza','pudin'
+
