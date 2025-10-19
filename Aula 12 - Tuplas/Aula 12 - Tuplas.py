67d47a108f4ad7ea803c08ca734cbb079db5eb46
Marcelo Augusto <marcelo.augusto.devcode@gmail.com>
2025-10-19 10:31:02 -0300

    initial commit

--- Aula 12 - Tuplas/Aula 12 - Tuplas.py
+++ Aula 12 - Tuplas/Aula 12 - Tuplas.py
@@ -1,8 +1,27 @@
-# Variavés composta: TUPLAS
+# Variavés composta: TUPLAS as TUPLAS são imutáveis.
 #variavel simples:
-lanche = Hamburgue
+lanche1 = 'Hamburgue'
 
 #variavel composta a tuplas, lista e dicionario : vamos usar tupla
 
-lanche1 = 'hamburgue', 'suco','pizza','pudin'
+lanche = ('hamburgue', 'suco','pizza','pudin')
 
+print(lanche)
+print(lanche[1])# e o suco
+print(lanche[-2])# pizza
+print(lanche[1:3])# suco e pizza
+print(lanche[:2])# hamburgue e suco
+print(lanche[-2:])# pizza e pudin
+print(lanche[2]) #ele vai mostra pizza
+print(lanche[0:2]) # ele vai mostra hamburgue, suco no python vai ate 0 1 o 2 nao mostra.
+print(lanche[1:])# vai do suco ate o pudin
+print(len(lanche)) # quantos elementos tem no lanche que e 4
+
+
+
+# com estrutura for.
+for c in lanche: #
+    print(c)
+
+for cont in range(0, len(lanche)):
+    print(cont)
