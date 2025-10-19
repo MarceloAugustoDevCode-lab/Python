732c3098a6f172c37e01f9163cf4b3c09a0e3561
Marcelo Augusto <marcelo.augusto.devcode@gmail.com>
2025-10-19 10:46:01 -0300

    initial commit

--- Aula 12 - Tuplas/Aula 12 - Tuplas.py
+++ Aula 12 - Tuplas/Aula 12 - Tuplas.py
@@ -12,6 +12,7 @@ print(lanche[-2])# pizza
 print(lanche[1:3])# suco e pizza
 print(lanche[:2])# hamburgue e suco
 print(lanche[-2:])# pizza e pudin
+print(lanche[-3])# pudin
 print(lanche[2]) #ele vai mostra pizza
 print(lanche[0:2]) # ele vai mostra hamburgue, suco no python vai ate 0 1 o 2 nao mostra.
 print(lanche[1:])# vai do suco ate o pudin
@@ -25,3 +26,19 @@ for c in lanche: #
 
 for cont in range(0, len(lanche)):
     print(cont)
+
+for cont1 in range(0, len(lanche)):
+    print(lanche[cont1])
+
+# contando a posição:
+for cont2 in range(0, len(lanche)):
+    print(f'Eu vou comer {lanche[cont2]} na posição {cont2}')
+
+#ou desse jeito.
+for pos,comida in enumerate(lanche):
+    print(f'Eu vou comer {comida} na posição {pos}')
+
+# o sorted coloca em ordem alfabetica.
+print(sorted(lanche))
+
+
