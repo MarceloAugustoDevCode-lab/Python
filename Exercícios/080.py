29aced33829f4c81dbfe33a56d6a6f9286bd9b74
Marcelo Augusto <marcelo.augusto.devcode@gmail.com>
2025-10-23 14:21:41 -0300

    initial commit

--- Exercícios/080.py
+++ Exercícios/080.py
@@ -1,12 +1,19 @@
 
 
 lista = []
+for c in range(0, 5):
+    n = int(input('Digite um valor: '))
+    lista.append(n)
 
-n = int(input('Digite um valor: '))
-lista.append(n)
-
-
-if n > 4 in lista:
-    lista.insert(3,n)
+    if n > 0 < 1 in lista:
+        lista.insert(0,n)
+    if n > 1 < 2 in lista:
+        lista.insert(1,n)
+    if n > 2 < 3 in lista:
+        lista.insert(2,n)
+    if n > 3 < 4 in lista:
+        lista.insert(3,n)
+    if n > 4 in lista:
+        lista.insert(4,n)
 
 print(lista)
