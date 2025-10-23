08679ced98bcf471d92f202d289f92c3b6fba28b
Marcelo Augusto <marcelo.augusto.devcode@gmail.com>
2025-10-23 13:31:13 -0300

    initial commit

--- Exercícios/080.py
+++ Exercícios/080.py
@@ -9,11 +9,11 @@ if n > 0 in lista:
     lista.insert(0,n)
 if n > 1 in lista:
     lista.insert(1,n)
-if n > 2 in lista:
+if n >= 2 in lista:
     lista.insert(2,n)
-if n > 4 in lista:
+if n < 3 in lista:
     lista.insert(3,n)
-if n > 5 in lista:
+if n > 4 in lista:
     lista.insert(4,n)
 
 print(lista)
